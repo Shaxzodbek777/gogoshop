b0df28fe33d322c3e809f79e19b709d50671c964
Shaxzodek777 <shaxzodbek206@gmail.com>
2024-05-05 11:03:03 +0300

    Gogo market

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -4,5 +4,69 @@ class AppStrings{
   static String v1 = 'api/v1';
 
   static String categoryList = '/category/get-categories';
+  static String categorySearch = '/category/get-search-categories';
+
+  static String activeSlider = '/slider/get-sliders';
+
+  static String blocksMainPage = '/general/get-blocks';
+  static String topMainPage = '/general/get-top-pages';
+  static String footerPage = '/general/get-footer-pages';
+  static String productInput = '/general/search-products-input/айпон';
+  static String contentPage = '/general/get-content/poryadok-registratsii';
+  static String popularBrand = '/general/get-popular-brands';
+  static String district = '/general/get-districts?city_id=1';
+
+  static String productAlias = '/product/get-product/xiaomi-13t';
+  static String relatedProduct = '/product/get-related-products/75';
+
+  // login Api
+ static String loginPage = '/cabinet/login';
+ static String registarPage = '/cabinet/register';
+ static String accaunt = '/cabinet/verify-account';
+ static String userdata = '/cabinet/get-user-data';
+
+ // test request
+static String message = '/test/send-test-telegram-message';
+
+// documents
+static String savePersonal = '/cabinet/submit-personal-data';
+static String creativeStory = '/cabinet/create-store';
+static String allStory = '/cabinet/get-stores';
+static String personalData = '/cabinet/personal-data';
+static String organization = '/cabinet/organization-types';
+
+// prduct
+static String request = '/default/check-connection';
+
+// profile
+static String authidinfication = '/profile/authenticate';
+static String login = '/profile/login';
+static String recorsevPasword = '/profile/recover-password';
+static String userId = '/profile/verify-user-otp';
+static String newPassword = '/profile/set-new-password';
+static String userData = '/profile/get-user-data';
+static String userAdress = '/profile/get-user-addresses';
+static String futureProduct = '#######';
+static String removeFromFuture = '/profile/remove-feature';
+static String producttoFuture = '/profile/add-feature';
+static String productfeatryCopy = '/profile/check-feature';
+static String listToFuture = '/profile/add-feature-list';
+static String registaruser = '/profile/sign-up';
+static String verifyuser = '/profile/verify-user';
+static String myOrder = '######';
+static String order = '######';
+static String newadress = '/profile/add-address';
+static String editAdress = '/profile/edit-address';
+static String userProfile = '/profile/edit-profile';
+static String remoweAdress = '/profile/remove-address';
+static String adressbyId = '#######';
+static String removeAllfutureProduct = '/profile/remove-features';
+
+// order
+
+static String sumbetOrder = '/order/submit-order';
+static String getOrderItem = '/order/order-items';
+
+
   
 }
